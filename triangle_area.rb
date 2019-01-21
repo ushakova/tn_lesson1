b879df7902937cde851c5ddbccbf820dc6075f16
Yunna Ushakova <79104745091@yandex.ru>
2019-01-21 21:02:39 +0300

    not int

--- triangle_area.rb
+++ triangle_area.rb
@@ -7,4 +7,4 @@ altitude = gets.chomp.to_i
 raise 'Высота и основание должны быть положительными числами' unless base > 0 && altitude > 0
 
 h = 0.5 * base * altitude
-puts "Площадь треугольника равнa #{h.to_i}"
+puts "Площадь треугольника равнa #{h}"
