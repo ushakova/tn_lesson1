47188de7e2a41eb91b5014eae104cdad523e2056
Yunna Ushakova <79104745091@yandex.ru>
2019-01-29 17:39:13 +0300

    requested changes

--- lesson_5/instance_counter.rb
+++ lesson_5/instance_counter.rb
@@ -1,19 +1,23 @@
 module InstanceCounter
   def self.included(base)
     base.extend ClassMethods
-    base.send :include, InstanceMethods
+    base.include InstanceMethods
   end
 
   module InstanceMethods
     private
 
     def register_instance
-      counter = self.class.instances || 0
-      self.class.instances = counter + 1
+      self.class.instances ||= 0
+      self.class.instances += 1
     end
   end
 
   module ClassMethods
-    attr_accessor :instances
+    attr_writer :instances
+
+    def instances
+      @instances ||= 0
+    end
   end
 end
